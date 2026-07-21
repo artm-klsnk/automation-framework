729d8b140ede05c6294d440532560aebabcd3d55
Artem Kolesnyk <artem.kolesnyk@openvpn.net>
2026-07-21 19:34:25 +0300

    extended the LoginPage class with all the elements validation, as well as added login method; added the negative test-case for invalid credentials; created a fixture for a default state of the index page

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,24 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@fixtures/pages.fixture';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Swag Labs/);
 });
 
+test('all the page elements are present on the page', async ({ loginPage }) => {
+  await loginPage.goto();
+  await loginPage.expectPageElementsVisible();
+});
+
+test('login with valid credentials', async ({ loginPage, page }) => {
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'secret_sauce');
+  await expect(page).toHaveURL(/inventory/);
+});
+
+test('login with invalid credentials', async ({ loginPage }) => {
+  await loginPage.goto();
+  await loginPage.login('standard_user', 'invalid_password');
+  await loginPage.expectUnsuccessfulLogin();
+});
